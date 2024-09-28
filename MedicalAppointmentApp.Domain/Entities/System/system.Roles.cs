0b04951adfc6cdd9fb65ea101b117d64c366675e
Naomi Meran Padilla <20231514@itla.edu.do>
2024-09-28 00:02:14 -0400

    Naomi ,-,

---
+++ MedicalAppointmentApp.Domain/Entities/System/system.Roles.cs
@@ -0,0 +1,10 @@
+﻿using MedicalAppointmentApp.Domain.Base;
+
+namespace MedicalAppointmentApp.Domain.Entities.System
+{
+    public class System_Roles : BaseEntity
+    {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+    }
+}
