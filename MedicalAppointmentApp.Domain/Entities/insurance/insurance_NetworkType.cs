ab88373607a5a9cf4245b3728800d276c992d298
Naomi Meran Padilla <20231514@itla.edu.do>
2024-09-28 15:24:22 -0400

    Organizado

--- MedicalAppointmentApp.Domain/Entities/insurance/insurance_NetworkType.cs
+++ MedicalAppointmentApp.Domain/Entities/insurance/insurance_NetworkType.cs
@@ -1,9 +1,4 @@
 ﻿using MedicalAppointmentApp.Domain.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MedicalAppointmentApp.Domain.Entities.insurance
 {
